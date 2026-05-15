c59aeaca38e07166cf231c31a33cb6bbaaf04f58
PON KISHORE K <kponkishore@gmail.com>
2026-05-15 19:50:45 +0530

    render deployment fix

--- backend/app.py
+++ backend/app.py
@@ -35,4 +35,5 @@ def create_app():
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=False, host='127.0.0.1', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
